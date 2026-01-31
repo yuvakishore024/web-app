7f1bcaf9716edef7c14dcb7c5574fa8f54482ab5
Yuvakishore <yuvakishore.024@gmail.com>
2026-01-31 16:43:40 +0530

    Add 3D tilt effect to cards on mousemove

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,21 @@
 document.querySelector("button").addEventListener("click", () => {
     alert("Welcome to Sensei Tech 🚀");
 });
+
+// 3D card tilt
+document.querySelectorAll(".tilt").forEach(card => {
+    card.addEventListener("mousemove", e => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const rotateY = ((x / rect.width) - 0.5) * 20;
+        const rotateX = ((y / rect.height) - 0.5) * -20;
+
+        card.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "rotateY(0deg) rotateX(0deg)";
+    });
+});
